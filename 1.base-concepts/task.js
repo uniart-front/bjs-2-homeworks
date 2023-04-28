efb0f48a77085db5664a19af03bff121aa4de653
uniart-front <109544062+uniart-front@users.noreply.github.com>
2023-04-28 16:06:28 +0500

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let D = (b ** 2) - (4 * a * c);
+    let arr = [];
+
+    if (a == 0) {
+        return arr;
+    }
+    if (D == 0) {
+        arr.push(-b / (2*a))
+    }
+    else if (D > 0) {
+        arr.push((-b + Math.sqrt(D)) / (2 * a))
+        arr.push((-b - Math.sqrt(D)) / (2 * a))
+    }
+    return arr;
 }
 
+console.log(solveEquation(1, -4, -5));
+
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+        return false;
+    }
+    let monthlyPercentage = ((percent/100)/12);
+    let loanBody = (amount-contribution);
+    let payment = (loanBody*(monthlyPercentage+(monthlyPercentage/(((1+monthlyPercentage)**countMonths)-1))));
+    let amountToBeRefunded = (payment*countMonths);
+    return +(amountToBeRefunded.toFixed(2));
 }
+    
+console.log(calculateTotalMortgage(10, 0, 20000, 24.));
